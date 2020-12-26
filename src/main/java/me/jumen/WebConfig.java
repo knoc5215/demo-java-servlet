8fdbdef273f85f9fed3a892c31700e375464b6a1
knoc5215 <knoc5215@naver.com>
2020-12-26 14:09:12 +0900

    스프링 MVC 빈 설정

--- src/main/java/me/jumen/WebConfig.java
+++ src/main/java/me/jumen/WebConfig.java
@@ -3,7 +3,13 @@ package me.jumen;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
@@ -16,5 +22,19 @@ public class WebConfig {
         viewResolver.setPrefix("/WEB-INF/");
         viewResolver.setSuffix((".jsp"));
         return viewResolver;
+    }
+
+    @Bean
+    public HandlerMapping handlerMapping() {
+        RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
+        handlerMapping.setInterceptors();
+        handlerMapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return handlerMapping;
+    }
+
+    @Bean
+    public HandlerAdapter handlerAdapter() {
+        RequestMappingHandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
+//        handlerAdapter.setArgumentResolvers();
     }
 }
